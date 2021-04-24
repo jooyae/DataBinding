b9ba5af4a3f34f5aa13404ccbdb63fdd0043c723
JooYae <jooyae3970@naver.com>
2021-04-24 17:40:24 +0900

    retrofit2: UiState

---
+++ app/src/main/java/org/sopt/databindingexercise/UiState.kt
@@ -0,0 +1,21 @@
+package org.sopt.databindingexercise
+
+
+data class UiState<out T>(
+    val status: Status,
+    val data: T?,
+    val message: String?
+) {
+    enum class Status{
+        SUCCESS,
+        LOADING,
+        ERROR
+    }
+
+    companion object {
+        fun <T> success(data: T?): UiState<T>  = UiState(Status.SUCCESS, data, null)
+        fun <T> loading(data: T?): UiState<T>  = UiState(Status.LOADING, data, null)
+        fun <T> error(data: T?, message: String?): UiState<T> = UiState(Status.ERROR, data, message)
+    }
+
+}
